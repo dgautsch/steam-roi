79b3c34b2b39ca6a81af0a35e4314e0c73fd052e
Dan Gautsch <dgautsch@gmail.com>
2018-01-24 01:38:41 -0500

    DRYing this up.

--- server/routes/steam.js
+++ server/routes/steam.js
@@ -3,9 +3,23 @@ const SteamApi = require('../lib/steamapi')
 const steam = new SteamApi(process.env.STEAM_API_KEY)
 const auth = require('../middleware/auth')
 
+function getUserOwnedGames (id, req, res) {
+  if (id) {
+    steam.getUserOwnedGames(req.user.id).then(data => {
+      res.status(200).json(req.user)
+    }, err => {
+      res.status(404).json('No games found.')
+      console.error(err)
+    })
+  } else {
+    res.status(404).json('User not found.')
+  }
+}
+
 r.get('/api/v1/user', function (req, res) {
   steam.resolve('https://steamcommunity.com/id/' + req.query.vanity).then(id => {
     if (id) {
+      console.log(id)
       steam.getUserOwnedGames(id).then(data => {
         res.status(200).json(data)
       }, err => {
@@ -19,27 +33,9 @@ r.get('/api/v1/user', function (req, res) {
 })
 
 r.get('/api/v1/user', auth.isAuthenticated, function (req, res) {
-  if (req.user.id) {
-    steam.getUserOwnedGames(req.user.id).then(data => {
-      res.status(200).json(req.user)
-    }, err => {
-      res.status(404).json('No games found.')
-      console.error(err)
-    })
-  } else {
-    res.status(200).json('User not found.')
-  }
+  getUserOwnedGames(req.user.id)
 })
 
 r.get('/api/v1/user', function (req, res) {
-  if (req.query.id) {
-    steam.getUserOwnedGames(req.query.id).then(data => {
-      res.status(200).json(data)
-    }, err => {
-      res.status(404).json('No games found.')
-      console.error(err)
-    })
-  } else {
-    res.status(404).json('User ID not found.')
-  }
+  getUserOwnedGames(req.query.id)
 })
