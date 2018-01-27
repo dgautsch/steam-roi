f6bf554604f7801b868e8037ab3e3ff09c364252
Dan Gautsch <dgautsch@gmail.com>
2018-01-27 00:33:01 -0500

    fixing mobile links

--- public/components/navbar.jsx
+++ public/components/navbar.jsx
@@ -10,14 +10,14 @@ class NavBar extends React.Component {
             <a href='/' data-activates='mobile-demo' className='button-collapse'><i className='material-icons'>menu</i></a>
             <ul id='nav-mobile' className='right hide-on-med-and-down'>
               <li><a href='/'>Home</a></li>
-              <li><a href='account'>Account</a></li>
-              <li><a href='search'>Search</a></li>
+              <li><a href='/account'>Account</a></li>
+              <li><a href='/search'>Search</a></li>
             </ul>
 
             <ul className='side-nav' id='mobile-demo'>
               <li><a href='/'>Home</a></li>
-              <li><a href='account'>Account</a></li>
-              <li><a href='search'>Search</a></li>
+              <li><a href='/account'>Account</a></li>
+              <li><a href='/search'>Search</a></li>
             </ul>
           </div>
         </nav>
