e2eee3fe135618f1ef280893acfcffbc76678a0b
Dan Gautsch <dgautsch@users.noreply.github.com>
2020-01-28 22:05:43 -0500

    redirect to home on login

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -7,7 +7,7 @@ function ensureAuthenticated (req, res, next) {
   if (req.isAuthenticated()) {
     return next()
   }
-  res.redirect('/')
+  res.redirect('/login')
 }
 module.exports = {
   isAuthenticated: function (req, res, next) {
